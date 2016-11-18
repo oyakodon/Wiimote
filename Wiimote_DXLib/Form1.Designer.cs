f642495240631e5962b80bf5ff88d2eaff284752
Oyakodon <oykdn2109@users.noreply.github.com>
2016-11-19 08:29:37 +0900

    ゆかりさんぐりぐりの実装完了

--- Wiimote_DXLib/Form1.Designer.cs
+++ Wiimote_DXLib/Form1.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Wiimote_DXLib/Form1.cs
+++ Wiimote_DXLib/Form1.cs
@@ -1,18 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using DxLibDLL;
+using WiimoteLib;
 
 namespace Wiimote_DXLib
 {
     public partial class Form1 : Form
     {
+        private Wiimote mWiimote;
+        private delegate void UpdateWiimoteStateDelegate(WiimoteChangedEventArgs args);
+        private delegate void UpdateExtensionChangedDelegate(WiimoteExtensionChangedEventArgs args);
+        private WiimoteLib.ButtonState previousBtns;
+        private WiimoteLib.NunchukState previousNunchuk;
+        private bool wmActivated = false;
+        private int speed = 200;
+
+        private　DX.VECTOR Model;
+        private DX.VECTOR Rotate;
+
         public Form1()
         {
             InitializeComponent();
@@ -26,16 +32,125 @@ namespace Wiimote_DXLib
             DX.SetAlwaysRunFlag(DX.TRUE);
             DX.DxLib_Init();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Wiimote初期化
+            mWiimote = new Wiimote();
+
+            try
+            {
+                mWiimote.WiimoteChanged += (s, args) => UpdateState(args);
+                mWiimote.WiimoteExtensionChanged += (s, args) => UpdateExtension(args);
+                mWiimote.Connect();
+                mWiimote.SetReportType(InputReport.IRExtensionAccel, true);
+                mWiimote.SetLEDs(true, false, false, false);
+
+            }
+            catch (WiimoteNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message, "Wiimote not found error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            catch (WiimoteException ex)
+            {
+                MessageBox.Show(ex.Message, "Wiimote error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Unknown error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+
+            // DX
+            Model.x = Model.y = Model.z = 0.0f;
+        }
+
+        public void UpdateState(WiimoteChangedEventArgs args)
+        {
+            BeginInvoke(new UpdateWiimoteStateDelegate(UpdateWiimoteChanged), args);
+        }
+
+        public void UpdateExtension(WiimoteExtensionChangedEventArgs args)
+        {
+            BeginInvoke(new UpdateExtensionChangedDelegate(UpdateExtensionChanged), args);
+        }
+
+        private void UpdateWiimoteChanged(WiimoteChangedEventArgs args)
+        {
+            WiimoteState ws = args.WiimoteState;
+
+            #region 判定
+            // 加速度センサによるモデル
+            if (previousBtns.A && !ws.ButtonState.A)
+            {
+                wmActivated = !wmActivated;
+            }
+
+            if (previousBtns.Minus && !ws.ButtonState.Minus)
+            {
+                speed += 100;
+            }
+
+            if (previousBtns.Plus && !ws.ButtonState.Plus)
+            {
+                speed -= 100;
+                if (speed < 200) speed = 200;
+            }
+
+            if (wmActivated)
+            {
+                Rotate.y -= (float)(Math.Round(ws.AccelState.Values.X * 20) / speed);
+                Rotate.x += (float)(Math.Round(ws.AccelState.Values.Y * 20) / speed);
+            }
+
+            if (ws.ButtonState.Home)
+            {
+                this.Close();
+            }
+
+            switch (ws.ExtensionType)
+            {
+                case ExtensionType.Nunchuk:
+                    var joy = ws.NunchukState.Joystick;
+                    if (joy.X > 0.3) Model.x -= 0.25f;
+                    if (joy.X < -0.3) Model.x += 0.25f;
+
+                    if (joy.Y > 0.3) Model.z -= 0.25f;
+                    if (joy.Y < -0.3) Model.z += 0.25f;
+
+                    if (ws.NunchukState.C) Model.y -= 0.25f;
+                    if (ws.NunchukState.Z) Model.y += 0.25f;
+
+                    previousNunchuk = ws.NunchukState;
+                    break;
+                    
+            }
+
+            previousBtns = ws.ButtonState;
+            #endregion
+
+        }
+
+        private void UpdateExtensionChanged(WiimoteExtensionChangedEventArgs args)
+        {
+            if (args.Inserted)
+                mWiimote.SetReportType(InputReport.IRExtensionAccel, true);
+            else
+                mWiimote.SetReportType(InputReport.IRAccel, true);
+        }
+
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
+            mWiimote.SetLEDs(false, false, false, false);
+            mWiimote.Disconnect();
+
             DX.DxLib_End();
         }
 
         public void DXUpdate()
         {
-            DX.VECTOR Model;
-            Model.x = Model.y = Model.z = 0.0f;
-
             int mHandle = DX.MV1LoadModel("Yukari/結月ゆかり_純_ver1.0.pmd");
             var keys = new byte[256];
 
@@ -45,14 +160,9 @@ namespace Wiimote_DXLib
             {
                 DX.ClearDrawScreen();
 
+                #region DXLib_キー入力
                 DX.GetHitKeyStateAll(out keys[0]);
-                
-                if (keys[DX.KEY_INPUT_ESCAPE] != 0)
-                {
-                    break;
-                }
-
-                DX.SetCameraPositionAndTarget_UpVecY(DX.VGet(0, 10, -20), DX.VGet(0.0f, 10.0f, 0.0f));
+                if (keys[DX.KEY_INPUT_ESCAPE] != 0) break;
 
                 if (keys[DX.KEY_INPUT_RIGHT] != 0) Model.x += 0.1f;
                 if (keys[DX.KEY_INPUT_LEFT] != 0) Model.x -= 0.1f;
@@ -62,14 +172,17 @@ namespace Wiimote_DXLib
 
                 if (keys[DX.KEY_INPUT_SPACE] != 0) Model.y -= 0.1f;
                 if (keys[DX.KEY_INPUT_LSHIFT] != 0) Model.y += 0.1f;
+                #endregion
+
+                DX.SetCameraPositionAndTarget_UpVecY(DX.VGet(0, 10, -20), DX.VGet(0.0f, 10.0f, 0.0f));
 
                 DX.MV1SetPosition(mHandle, Model);
+                DX.MV1SetRotationXYZ(mHandle, Rotate);
                 DX.MV1DrawModel(mHandle);
 
                 DX.ScreenFlip();
                 Application.DoEvents();
             }
         }
-
     }
 }
